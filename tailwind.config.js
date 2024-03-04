a4da7796006a7a20ce25259ecb3e2383a4eb2003
Jimmy772 <bobcook772@outlook.com>
2024-03-04 18:34:04 +0300

    chore: added colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,19 @@ module.exports = {
       'xl': '1440px',
       // => @media (min-width: 1440px) { ... }
     },
+    colors: {
+      'primary': {
+        'purple': 'hsl(259, 100%, 65%)',
+        'red': 'hsl(0, 100%, 67%)'
+      },
+      'neutral': {
+        'white': 'hsl(0, 0%, 100%)',
+        'off-white': 'hsl(0, 0%, 94%)',
+        'grey': 'hsl(0, 0%, 86%)',
+        'smoke': 'hsl(0, 1%, 44%)',
+        'black': 'hsl(0, 0%, 8%)'
+      }
+    },
     extend: {},
   },
   plugins: [],
